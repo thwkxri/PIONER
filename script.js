6348abdeec87b491e92c0f8312bc4c376234cdc5
Dmitry <shoex64@gmail.com>
2024-04-30 17:52:35 +0300

    overlow

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-document.getElementById('burger').addEventListener('click', () => {
+document.getElementById('burger').addEventListener('click', function() {
   document.querySelector('header').classList.toggle('open');
-  if(document.querySelector('header').classList.contains('open')){
+
+  if (document.querySelector('header').classList.contains('open')) {
     document.body.style.overflow = 'hidden';
+
+    // Заблокируем скроллинг на мобильных устройствах
+    document.addEventListener('touchmove', preventDefault, { passive: false });
   } else {
     document.body.style.overflow = 'visible';
+
+    // Уберем блокировку скроллинга на мобильных устройствах
+    document.removeEventListener('touchmove', preventDefault, { passive: false });
   }
 });
+
+function preventDefault(e) {
+  e.preventDefault();
+}
