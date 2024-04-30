a32aeb48729e084c1f1fe3d018f36f31b22b685d
Dmitry <shoex64@gmail.com>
2024-04-30 16:09:11 +0300

    THATS A

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.getElementById('burger').addEventListener('click', () => {
   document.querySelector('header').classList.toggle('open');
- 
-})
+  if(document.querySelector('header').classList.contains('open')){
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = 'visible';
+  }
+});
